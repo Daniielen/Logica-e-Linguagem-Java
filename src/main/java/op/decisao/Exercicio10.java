4aa216adc749db9bfbdd072eded8ba428ba12158
daniielen <danielensantana14@gmail.com>
2023-09-19 20:29:31 -0300

    ✨ [Lista 1] Exercícios 9 e 10

---
+++ src/main/java/op/decisao/Exercicio10.java
@@ -0,0 +1,17 @@
+package op.decisao;
+/**
+ * @author Danielen Santana
+ * @details Dadas idades de quatro pessoas, encontre a média das idades das pessoas
+ * nascidas em ano bissexto. Uma mensagem deve ser dada caso alguma idade não
+ * seja positiva. Deve-se considerar o ano atual como referência para descoberta de
+ * ano bissexto.
+ * @note Prática: Operação de Decisão na linguagem Java.
+ * Exercício criado pelo Prof Dr.Josildo Silva para o curso em
+ * Lógica e Linguagem de Programação pelo IFBA - campus Camaçari.
+ * @since 18/09/2023
+ */
+public class Exercicio10 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/op/decisao/Exercicio9.java
@@ -0,0 +1,21 @@
+package op.decisao;
+
+/**
+ * @author Danielen Santana
+ * @details Dado nome e ano de nascimento de três pessoas, uma vez que o ano de nascimento
+ * seja positivo, deve-se informar se a pessoa mais jovem possui ou não o mesmo
+ * nome da pessoa mais velha.
+ * @note Prática: Operação de Decisão na linguagem Java.
+ * Exercício criado pelo Prof Dr.Josildo Silva para o curso em
+ * Lógica e Linguagem de Programação pelo IFBA - campus Camaçari.
+ * @since 18/09/2023
+ */
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+        // { ♥ } - Entrada de Dados e Declaração de Variáveis
+        String nome_1, nome_2, nome_3, resposta;
+
+        
+    }
+}
